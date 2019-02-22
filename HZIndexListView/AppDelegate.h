e4c49aa0d7ed1b1999daedecb8ca2277b2e5af1b
heze <895315401@qq.com>
2019-02-22 22:22:32 +0800

    Initial Commit

---
+++ HZIndexListView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HZIndexListView
+//
+//  Created by 何泽的mac on 2019/2/22.
+//  Copyright © 2019年 何泽的mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HZIndexListView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HZIndexListView
+//
+//  Created by 何泽的mac on 2019/2/22.
+//  Copyright © 2019年 何泽的mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
